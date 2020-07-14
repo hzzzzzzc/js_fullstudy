c090aa88d7c0852d08b965f13984122aa82ba97c
hzzzzzzc <1292153062@qq.com>
2020-07-14 12:05:14 +0800

    filbird

--- html/FLYBIRD/js/bird.js
+++ html/FLYBIRD/js/bird.js
@@ -15,5 +15,49 @@ var bird = {
         this.div.style.zIndex = '1'
         //将小鸟div容器插入到游戏界面当中
         parentobj.appendChild(this.div)
+    },
+
+    fallSpeed: 0,   // 小鸟下落速度
+    flyBird: function() {    // 控制小鸟飞翔下落的函数
+        this.flyTimer = setInterval(fly, 60)
+        function fly() {
+            bird.div.style.top = bird.div.offsetTop + bird.fallSpeed++ +'px'
+            if (bird.div.offsetTop >= 395) {      // 掉到地面，清除定时器
+                bird.fallSpeed = 0
+                clearInterval(bird.flyTimer)
+                clearInterval(bird.wingTimer)
+            }
+            if (bird.fallSpeed > 12) {   // 控制小鸟下落最大速度不超过12
+                bird.fallSpeed = 12
+            }
+            if(bird.div.offsetTop <= 0)  {
+                bird.div.style.top = 0
+                bird.fallSpeed = 2
+
+            }
+
+        }
+
+    },
+    
+    wingWave: function() {     // 控制小鸟扇动翅膀的函数
+        var up = ['url(img/up_bird0.png', 'url(img/up_bird1.png']
+        var down = ['url(img/down_bird0.png', 'url(img/down_bird1.png']
+        bird. wingTimer = setInterval(wing, 120)
+        var i = 0, j = 0
+        function wing() {
+            if (bird.fallSpeed > 0) {
+                bird.div.style.backgroundImage = down[i++]
+                if (i === 2) {
+                    i = 0
+                }
+            }else {
+                bird.div.style.backgroundImage = up[j++]
+                if (j === 2) {
+                    j = 0
+                }
+            }
+        }
+
     }
 }
