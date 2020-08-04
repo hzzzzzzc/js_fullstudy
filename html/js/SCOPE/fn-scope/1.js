379278e201e3e70e2f69ea4bc3ced71e2ab0a048
hzzzzzzc <1292153062@qq.com>
2020-08-04 11:24:06 +0800

    note

---
+++ html/js/SCOPE/fn-scope/1.js
@@ -0,0 +1,28 @@
+
+// function foo(a) {
+//     var b;
+//     b = a + bar(a * 2)
+//     function bar(a) {
+//         return a - 1
+//     }
+//     console.log(b * 3)
+// }
+
+
+// foo(2)
+
+// function foo() {
+
+//     function bar(a) {
+//         i = 3
+//     }
+// }
+
+// 自执行函数
+
+var a = 2;
+(function() {
+    var a = 3
+    console.log(a)
+})()
+console.log(a*2)
